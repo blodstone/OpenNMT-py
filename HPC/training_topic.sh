50717689864ae975d2e7c3de2e7eef7c751b317d
blodstone <hardy.oei@gmail.com>
2019-05-08 21:09:21 +0100

    Change HPC script

--- HPC/training_topic.sh
+++ HPC/training_topic.sh
@@ -3,7 +3,7 @@
 #$ -P rse
 #$ -q rse.q
 #$ -l rmem=8G
-#$ -l h_rt=8:00:00
+#$ -l h_rt=24:00:00
 #$ -M hhardy2@sheffield.ac.uk
 #$ -m easb
 #$ -wd /home/acp16hh/Topic_Summ
@@ -16,7 +16,7 @@ module load apps/python/conda
 module load libs/cudnn/7.3.1.20/binary-cuda-9.0.176
 source activate freya
 
-python preprocess.py -train_src $DATA/src.train.token -train_topic $DATA/src.lda.train -train_lemma $DATA/src.train.lemma -train_tgt $DATA/tgt.txt.train -valid_src $DATA/src.validation.token -valid_tgt $DATA/tgt.txt.validation -valid_topic $DATA/src.lda.validation -valid_lemma $DATA/src.validation.lemma -save_data $DATA/topic-bbc-split -src_seq_length 10000 -tgt_seq_length 10000 -src_seq_length_trunc 400 -tgt_seq_length_trunc 100 -dynamic_dict -share_vocab -shard_size 100000
+#python preprocess.py -train_src $DATA/src.train.token -train_topic $DATA/src.lda.train -train_lemma $DATA/src.train.lemma -train_tgt $DATA/tgt.txt.train -valid_src $DATA/src.validation.token -valid_tgt $DATA/tgt.txt.validation -valid_topic $DATA/src.lda.validation -valid_lemma $DATA/src.validation.lemma -save_data $DATA/topic-bbc-split -src_seq_length 10000 -tgt_seq_length 10000 -src_seq_length_trunc 400 -tgt_seq_length_trunc 100 -dynamic_dict -share_vocab -shard_size 100000
 
 python train.py -save_model $MODEL/topic-bbc-split -data $DATA -global_attention mlp -word_vec_size 256 -rnn_size 1024 -layers 2 -encoder_type brnn -train_steps 200000 -max_grad_norm 2 -dropout 0.2 -batch_size 16 -valid_batch_size 16 -optim adagrad -learning_rate 0.15 -adagrad_accumulator_init 0.1 -seed 777 -topic_attn -topic_matrix $DATA/topic_matrix.tensor -lemma-align $DATA/src.train.pair -bridge -world_size 1 -gpu_ranks 0 -log_file $OUTPUT/train_topic_01.log
 
