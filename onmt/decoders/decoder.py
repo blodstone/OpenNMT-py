Fixed bug with the topic matrix construction, scaling up the topic matrix result, and use standard averaging for probability distribution mixture

onmt/decoders/decoder.py
@@ -85,7 +85,7 @@ class RNNDecoderBase(DecoderBase):
     def __init__(self, rnn_type, bidirectional_encoder, num_layers,
                  hidden_size, attn_type="general", attn_func="softmax",
                  coverage_attn=False, context_gate=None,
-                 copy_attn=False, dropout=0.0, embeddings=None, text_field=None,
+                 copy_attn=False, dropout=0.0, embeddings=None,
                  reuse_copy_attn=False, copy_attn_type="general"):
         super(RNNDecoderBase, self).__init__(
             attentional=attn_type != "none" and attn_type is not None)
@@ -94,7 +94,6 @@ class RNNDecoderBase(DecoderBase):
         self.num_layers = num_layers
         self.hidden_size = hidden_size
         self.embeddings = embeddings
-        self.text_field = text_field
         self.dropout = nn.Dropout(dropout)
 
         # Decoder state
@@ -106,12 +105,7 @@ class RNNDecoderBase(DecoderBase):
                                    hidden_size=hidden_size,
                                    num_layers=num_layers,
                                    dropout=dropout)
-        vocab_size = len(self.text_field.base_field.vocab)
-        self.generator = nn.Sequential(
-            nn.Linear(self.hidden_size, vocab_size),
-            Cast(torch.float32),
-            nn.Softmax(dim=-1)
-        )
+
         # Set up the context gate.
         self.context_gate = None
         if context_gate is not None:
@@ -147,7 +141,7 @@ class RNNDecoderBase(DecoderBase):
             raise ValueError("Cannot reuse copy attention with no attention.")
 
     @classmethod
-    def from_opt(cls, opt, embeddings, text_field):
+    def from_opt(cls, opt, embeddings):
         """Alternate constructor."""
         return cls(
             opt.rnn_type,
@@ -161,7 +155,6 @@ class RNNDecoderBase(DecoderBase):
             opt.copy_attn,
             opt.dropout,
             embeddings,
-            text_field,
             opt.reuse_copy_attn,
             opt.copy_attn_type)
 
@@ -405,21 +398,18 @@ class InputFeedRNNDecoder(RNNDecoderBase):
         for idx_t, emb_t in enumerate(emb.split(1)):
             decoder_input = torch.cat([emb_t.squeeze(0), input_feed], 1)
             rnn_output, dec_state = self.rnn(decoder_input, dec_state)
-            # self.generator[0].weight.requires_grad = False
-            # self.generator[0].bias.requires_grad = False
-            # rnn_topic = torch.multinomial(self.generator(rnn_output), 1)
-            # self.generator[0].weight.requires_grad = True
-            # self.generator[0].bias.requires_grad = True
             rnn_topic = tgt[idx_t]
             rnn_topic[rnn_topic > (topic_matrix.size(0) - 1)] = 0
-            rnn_topic = topic_matrix[word_to_lemma[rnn_topic]]
+            rnn_topic = topic_matrix[rnn_topic]
             if self.attentional:
                 decoder_output, p_attn, t_attn = self.attn(
                     rnn_output,
                     memory_bank.transpose(0, 1),
                     rnn_topic,
                     topic_memory_bank.transpose(0, 1),
-                    memory_lengths=memory_lengths)
+                    memory_lengths=memory_lengths,
+                    is_UNK_topic=torch.eq(topic_matrix[0], rnn_topic).all()
+                )
                 attns["std"].append(p_attn)
                 attns["topic"].append(t_attn)
             else:

onmt/modules/topic_attention.py
@@ -80,15 +80,17 @@ class TopicAttention(nn.Module):
         assert attn_func in ["softmax", "sparsemax"], (
             "Please select a valid attention function.")
         self.attn_func = attn_func
-        self.linear_topic_hidden = nn.Linear(512, self.dim, bias=True)
-        self.linear_topic = nn.Linear(2*dim, dim, bias=True)
 
         if self.attn_type == "general":
             self.linear_in = nn.Linear(dim, dim, bias=False)
+            self.linear_in_topic = nn.Linear(512, 512, bias=False)
         elif self.attn_type == "mlp":
             self.linear_context = nn.Linear(dim, dim, bias=False)
             self.linear_query = nn.Linear(dim, dim, bias=True)
             self.v = nn.Linear(dim, 1, bias=False)
+            self.linear_context_topic = nn.Linear(512, 512, bias=False)
+            self.linear_query_topic = nn.Linear(512, 512, bias=True)
+            self.v_topic = nn.Linear(512, 1, bias=False)
         # mlp wants it with bias
         out_bias = self.attn_type == "mlp"
         self.linear_out = nn.Linear(dim * 2, dim, bias=out_bias)
@@ -112,7 +114,6 @@ class TopicAttention(nn.Module):
         tgt_batch, tgt_len, tgt_dim = h_t.size()
         aeq(src_batch, tgt_batch)
         aeq(src_dim, tgt_dim)
-        aeq(self.dim, src_dim)
 
         if self.attn_type in ["general", "dot"]:
             if self.attn_type == "general":
@@ -137,7 +138,47 @@ class TopicAttention(nn.Module):
 
             return self.v(wquh.view(-1, dim)).view(tgt_batch, tgt_len, src_len)
 
-    def forward(self, source, memory_bank, source_topic, topic_bank,
+    def score_topic(self, h_t, h_s):
+        """
+        Args:
+          h_t (FloatTensor): sequence of queries ``(batch, tgt_len, dim)``
+          h_s (FloatTensor): sequence of sources ``(batch, src_len, dim``
+
+        Returns:
+          FloatTensor: raw attention scores (unnormalized) for each src index
+            ``(batch, tgt_len, src_len)``
+        """
+
+        # Check input sizes
+        src_batch, src_len, src_dim = h_s.size()
+        tgt_batch, tgt_len, tgt_dim = h_t.size()
+        aeq(src_batch, tgt_batch)
+        aeq(src_dim, tgt_dim)
+
+        if self.attn_type in ["general", "dot"]:
+            if self.attn_type == "general":
+                h_t_ = h_t.view(tgt_batch * tgt_len, tgt_dim)
+                h_t_ = self.linear_in_topic(h_t_)
+                h_t = h_t_.view(tgt_batch, tgt_len, tgt_dim)
+            h_s_ = h_s.transpose(1, 2)
+            # (batch, t_len, d) x (batch, d, s_len) --> (batch, t_len, s_len)
+            return torch.bmm(h_t, h_s_)
+        else:
+            dim = 512
+            wq = self.linear_query_topic(h_t.view(-1, dim))
+            wq = wq.view(tgt_batch, tgt_len, 1, dim)
+            wq = wq.expand(tgt_batch, tgt_len, src_len, dim)
+
+            uh = self.linear_context_topic(h_s.contiguous().view(-1, dim))
+            uh = uh.view(src_batch, 1, src_len, dim)
+            uh = uh.expand(src_batch, tgt_len, src_len, dim)
+
+            # (batch, t_len, s_len, d)
+            wquh = torch.tanh(wq + uh)
+
+            return self.v_topic(wquh.view(-1, dim)).view(tgt_batch, tgt_len, src_len)
+
+    def forward(self, source, memory_bank, source_topic, topic_bank, is_UNK_topic=False,
                 memory_lengths=None, coverage=None, sample=None, fusion=None):
         """
 
@@ -180,21 +221,6 @@ class TopicAttention(nn.Module):
             memory_bank += self.linear_cover(cover).view_as(memory_bank)
             memory_bank = torch.tanh(memory_bank)
 
-        ## Topic alignment
-        hidden_source_topic = self.linear_topic_hidden(source_topic)
-        hidden_topic_bank = self.linear_topic_hidden(topic_bank)
-        topic_align = self.score(hidden_source_topic, hidden_topic_bank)
-        if memory_lengths is not None:
-            mask = sequence_mask(memory_lengths, max_len=topic_align.size(-1))
-            mask = mask.unsqueeze(1)  # Make it broadcastable.
-            topic_align.masked_fill_(1 - mask, -float('inf'))
-            if self.attn_func == "softmax":
-                topic_align_vectors = F.softmax(topic_align.view(batch * target_l, source_l), -1)
-            else:
-                topic_align_vectors = sparsemax(topic_align.view(batch * target_l, source_l), -1)
-            topic_align_vectors = topic_align_vectors.view(batch, target_l, source_l)
-            c_topic = torch.bmm(topic_align_vectors, memory_bank)
-
         ## Global alignment
         # compute attention scores, as in Luong et al.
         align = self.score(source, memory_bank)
@@ -214,10 +240,25 @@ class TopicAttention(nn.Module):
         # each context vector c_t is the weighted average
         # over all the source hidden states
         c = torch.bmm(align_vectors, memory_bank)
-
-        # concatenate topic context and context
-        c = self.linear_topic(torch.cat((c, c_topic), dim=2))
-
+        topic_align_vectors = align_vectors
+        if not is_UNK_topic:
+            ## Topic alignment
+            # Scaling by 10e7 to prevent buffer underflow
+            topic_align = self.score_topic(source_topic*10e7, topic_bank*10e7)
+            if memory_lengths is not None:
+                mask = sequence_mask(memory_lengths, max_len=topic_align.size(-1))
+                mask = mask.unsqueeze(1)  # Make it broadcastable.
+                topic_align.masked_fill_(1 - mask, -float('inf'))
+                if self.attn_func == "softmax":
+                    topic_align_vectors = F.softmax(topic_align.view(batch * target_l, source_l), -1)
+                else:
+                    topic_align_vectors = sparsemax(topic_align.view(batch * target_l, source_l), -1)
+                topic_align_vectors = topic_align_vectors.view(batch, target_l, source_l)
+                c_topic = torch.bmm(topic_align_vectors, memory_bank)
+            # concatenate topic context and context
+            # c = self.linear_topic(torch.cat((c, c_topic), dim=2))
+            theta = 0.5
+            c = theta*c + (1-theta)*c_topic
         # concatenate
         concat_c = torch.cat([c, source], 2).view(batch*target_l, dim*2)
         attn_h = self.linear_out(concat_c).view(batch, target_l, dim)
@@ -248,5 +289,4 @@ class TopicAttention(nn.Module):
             aeq(target_l, target_l_)
             aeq(batch, batch_)
             aeq(source_l, source_l_)
-
         return attn_h, align_vectors, topic_align_vectors

onmt/train_single.py
@@ -62,23 +62,6 @@ def main(opt, device_id):
         model_opt = opt
         vocab = torch.load(opt.data + '.vocab.pt')
 
-    logger.info('Loading alignment.')
-    lemma_aligns = open(model_opt.lemma_align, 'rb').readlines()
-    src_stoi = vocab['src'].base_field.vocab.stoi
-    lemma_stoi = vocab['word_topic'].base_field.vocab.stoi
-    w2l = {}
-    word_to_lemma = []
-    for pair in lemma_aligns:
-        pair = pair.strip().split()
-        w2l[src_stoi[pair[0].decode('utf-8')]] = \
-            lemma_stoi[pair[1].decode('utf-8').lower()]
-    w2l[src_stoi['unk']] = lemma_stoi['unk']
-    for index in range(len(vocab['src'].base_field.vocab.itos)):
-        if index in w2l:
-            word_to_lemma.append(w2l[index])
-        else:
-            word_to_lemma.append(w2l[lemma_stoi['unk']])
-    word_to_lemma = torch.tensor(word_to_lemma)
     logger.info('Loading topic matrix')
     if device_id >= 0:
         topic_matrix = torch.load(opt.topic_matrix, map_location=torch.device(device_id))
@@ -120,7 +103,6 @@ def main(opt, device_id):
 
     trainer = build_trainer(
         opt, device_id, model, fields, optim, model_saver=model_saver)
-
     train_iter = build_dataset_iter("train", fields, opt)
     valid_iter = build_dataset_iter(
         "valid", fields, opt, is_train=False)
@@ -135,7 +117,7 @@ def main(opt, device_id):
         train_steps = 0
     trainer.train(
         topic_matrix,
-        word_to_lemma,
+        dict(),
         train_iter,
         train_steps,
         save_checkpoint_steps=opt.save_checkpoint_steps,

topic_matrix_to_tensor.py
@@ -12,19 +12,48 @@ def main():
                         help="Output file for the prepared data")
     parser.add_argument('-dict_file', required=True,
                         help="Dictionary file")
+    parser.add_argument('-lemma_align', required=True,
+                        help="Pair dictionary")
     opt = parser.parse_args()
     fields = torch.load(opt.dict_file)
     try:
-        vocab = fields['word_topic'].base_field.vocab
+        lemma_vocab = fields['word_topic'].base_field.vocab
     except AttributeError:
-        vocab = fields['word_topic'].vocab
+        lemma_vocab = fields['word_topic'].vocab
+    tgt_vocab = fields['src'].base_field.vocab
     with open(opt.emb_file, 'rb') as f:
         embs = pickle.load(f)
+    lemma_aligns = open(opt.lemma_align, 'rb').readlines()
+    w2l = {}
+    for pair in lemma_aligns:
+        pair = pair.strip().split()
+        word = pair[0].decode('utf-8')
+        lemma = pair[1].decode('utf-8')
+        if word not in w2l:
+            w2l[word] = [lemma]
+        else:
+            w2l[word].append(lemma)
     dim = len(six.next(six.itervalues(embs)))
-    tensor = torch.zeros((len(vocab), dim))
-    for word, values in embs.items():
-        tensor[vocab.stoi[word]] = torch.Tensor(values)
+    tensor = torch.zeros((len(lemma_vocab), dim))
+    i = 1
+    words = []
+    for idx, word in enumerate(tgt_vocab.itos):
+        if word not in w2l:
+            tensor[idx] = torch.tensor(embs['UNK'])
+            words.append(word)
+            i += 1
+        else:
+            found = False
+            for w2l_word in w2l[word]:
+                if w2l_word in embs:
+                    tensor[idx] = torch.tensor(embs[w2l_word])
+                    found = True
+            if not found:
+                tensor[idx] = torch.tensor(embs['UNK'])
+                words.append(word)
+                i += 1
     torch.save(tensor, opt.output_file)
-
+    print(i)
+    print(words)
 if __name__ == '__main__':
     main()