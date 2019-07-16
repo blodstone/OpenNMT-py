97c6d26e84fd46b3ea9eebd4f033ee9ddf219ea4
blodstone <hardy.oei@gmail.com>
2019-07-16 01:19:53 +0100

    Added pooling mode in create train script

--- HPC/create_train_scripts.py
+++ HPC/create_train_scripts.py
@@ -28,6 +28,7 @@ if __name__ == '__main__':
     joint_attn_modes = ['co_attention', 'mix', 'embedded']
     thetas = [0.75, 0.5, 0.25]
     poolings = ['column', 'row', 'exp']
+    pooling_modes = ['max', 'mean']
     weighted_co_attns = [True, False]
     topic_attns = ['dot', 'mlp']
     topic_attn_functions = ['sparsemax', 'softmax']
@@ -36,25 +37,26 @@ if __name__ == '__main__':
     for joint_attn_mode in joint_attn_modes:
         if joint_attn_mode == 'co_attention':
             for pooling in poolings:
-                for topic_attn in topic_attns:
-                    for topic_attn_function in topic_attn_functions:
-                        for replace_unk_topic in replace_unk_topics:
-                            name = '{}_{}_{}_{}_{}'.format(
-                                joint_attn_mode, pooling,
-                                topic_attn, topic_attn_function, replace_unk_topic)
-                            log_file = '$OUTPUT/{}.log'.format(name)
-                            save_model = '$MODEL/{}/{}'.format(name, name)
-                            if not os.path.exists('/fastdata/acp16hh/models/{}'.format(name)):
-                                os.makedirs('/fastdata/acp16hh/models/{}'.format(name))
-                            if replace_unk_topic:
-                                body = 'python train.py -save_model {} -data {} -copy_attn -global_attention mlp -word_vec_size 256 -rnn_size 1024 -layers 2 -encoder_type brnn -train_steps 220000 -max_grad_norm 2 -dropout 0. -batch_size 8 -valid_batch_size 8 -optim adagrad -learning_rate 0.15 -adagrad_accumulator_init 0.1 -seed 777 -topic_matrix {} -bridge -world_size 0 -gpu_ranks 0 -reuse_copy_attn -copy_loss_by_seqlength -joint_attn_mode {} -pooling {} -topic_attn {} -topic_attn_function {} -log_file {} -replace_unk_topic'.format(save_model, data, topic_matrix, joint_attn_mode, pooling, topic_attn, topic_attn_function, log_file)
-                            else:
-                                body = 'python train.py -save_model {} -data {} -copy_attn -global_attention mlp -word_vec_size 256 -rnn_size 1024 -layers 2 -encoder_type brnn -train_steps 220000 -max_grad_norm 2 -dropout 0. -batch_size 8 -valid_batch_size 8 -optim adagrad -learning_rate 0.15 -adagrad_accumulator_init 0.1 -seed 777 -topic_matrix {} -bridge -world_size 0 -gpu_ranks 0 -reuse_copy_attn -copy_loss_by_seqlength -joint_attn_mode {} -pooling {} -topic_attn {} -topic_attn_function {} -log_file {}'.format(save_model, data, topic_matrix, joint_attn_mode, pooling, topic_attn, topic_attn_function, log_file)
-                            file_path = os.path.join('topic/s{}_topic_{}.sh'.format(script_id, name))
-                            script_id += 1
-                            file = open(file_path, 'w')
-                            file.write(gen_header() + body)
-                            file.close()
+                for pooling_mode in pooling_modes:
+                    for topic_attn in topic_attns:
+                        for topic_attn_function in topic_attn_functions:
+                            for replace_unk_topic in replace_unk_topics:
+                                name = '{}_{}_{}_{}_{}'.format(
+                                    joint_attn_mode, pooling,
+                                    topic_attn, topic_attn_function, replace_unk_topic)
+                                log_file = '$OUTPUT/{}.log'.format(name)
+                                save_model = '$MODEL/{}/{}'.format(name, name)
+                                if not os.path.exists('/fastdata/acp16hh/models/{}'.format(name)):
+                                    os.makedirs('/fastdata/acp16hh/models/{}'.format(name))
+                                if replace_unk_topic:
+                                    body = 'python train.py -save_model {} -data {} -copy_attn -global_attention mlp -word_vec_size 256 -rnn_size 1024 -layers 2 -encoder_type brnn -train_steps 220000 -max_grad_norm 2 -dropout 0. -batch_size 8 -valid_batch_size 8 -optim adagrad -learning_rate 0.15 -adagrad_accumulator_init 0.1 -seed 777 -topic_matrix {} -bridge -world_size 0 -gpu_ranks 0 -reuse_copy_attn -copy_loss_by_seqlength -joint_attn_mode {} -pooling {} -pooling_mode {} -topic_attn {} -topic_attn_function {} -log_file {} -replace_unk_topic'.format(save_model, data, topic_matrix, joint_attn_mode, pooling, pooling_mode, topic_attn, topic_attn_function, log_file)
+                                else:
+                                    body = 'python train.py -save_model {} -data {} -copy_attn -global_attention mlp -word_vec_size 256 -rnn_size 1024 -layers 2 -encoder_type brnn -train_steps 220000 -max_grad_norm 2 -dropout 0. -batch_size 8 -valid_batch_size 8 -optim adagrad -learning_rate 0.15 -adagrad_accumulator_init 0.1 -seed 777 -topic_matrix {} -bridge -world_size 0 -gpu_ranks 0 -reuse_copy_attn -copy_loss_by_seqlength -joint_attn_mode {} -pooling {} -pooling_mode {} -topic_attn {} -topic_attn_function {} -log_file {}'.format(save_model, data, topic_matrix, joint_attn_mode, pooling, pooling_mode, topic_attn, topic_attn_function, log_file)
+                                file_path = os.path.join('topic/s{}_topic_{}.sh'.format(script_id, name))
+                                script_id += 1
+                                file = open(file_path, 'w')
+                                file.write(gen_header() + body)
+                                file.close()
         elif joint_attn_mode == 'mix':
             for theta in thetas:
                 for topic_attn in topic_attns:
